4a84f66a0b2e588c69c086f0e4f61aa12fb9d971
krishnavasavi06 <134129450+krishnavasavi06@users.noreply.github.com>
2023-06-10 01:15:21 +0530

    Add files via upload

---
+++ exp 46).c
@@ -0,0 +1,11 @@
+enter name for student 1: dinesh
+enter grades for student 1:
+	test 1:76
+	test 2:87
+	test 3:65
+	test 4:78
+enter name for student 2: divya
+    test 1:90
+    test 2:65
+    test 3:78
+    test 4:94
