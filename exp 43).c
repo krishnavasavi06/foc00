762a6d2ae59d086e9ab6505cd85e0d0ad82cb358
krishnavasavi06 <134129450+krishnavasavi06@users.noreply.github.com>
2023-06-10 00:58:02 +0530

    Add files via upload

---
+++ exp 43).c
@@ -0,0 +1,21 @@
+enter number of customers for day 1:
+hour 1:23
+hour 2:11
+hour 3:22
+hour 4:33
+hour 5:44
+hour 6:55
+hour 7:56
+hour 8:77
+hour 9:2
+hour 10:11
+hour 11:11
+hour 12:11
+hour 13:11
+hour 14:11
+hour 15:11
+hour 16:11
+hour 17:11
+hour 18:22
+hour 19:33
+hour 20:44
