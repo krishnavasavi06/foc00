32490dfc87e8e8bf3f112534042e07554c663b3d
krishnavasavi06 <134129450+krishnavasavi06@users.noreply.github.com>
2023-06-10 00:38:43 +0530

    Add files via upload

---
+++ exp 40).c
@@ -0,0 +1,7 @@
+enter starting odometer reading:322
+enter ododmeter reading for fillup 1:300
+enter number of gallons for fillup 1:350
+enter odometer reading for fillup 2:433
+enter number of gallons for fillup 2:543
+enter number of reading for fillup 3:65
+enter number of gallons for fillup 3:43
