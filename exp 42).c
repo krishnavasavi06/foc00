f1e4c3c12a45c0d215da2972f83fad2c66a4c225
krishnavasavi06 <134129450+krishnavasavi06@users.noreply.github.com>
2023-06-10 00:51:14 +0530

    Add files via upload

---
+++ exp 42).c
@@ -0,0 +1,14 @@
+enter correct score for question 1:1
+enter correct score for question 2:1
+enter correct score for question 3:1
+enter correct score for question 4:0
+enter correct score for question 5:0
+enter correct score for question 6:1
+enter correct score for question 7:0
+enter correct score for question 8:0
+enter correct score for question 9:1
+enter correct score for question 10:0
+enter correct score for question 11:0
+enter correct score for question 12:1
+enter correct score for question 13:1
+enter correct score for question 14:1
