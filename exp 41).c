b23c569830aa92e014c2b8580c12eab66ba7900d
krishnavasavi06 <134129450+krishnavasavi06@users.noreply.github.com>
2023-06-10 00:46:07 +0530

    Add files via upload

---
+++ exp 41).c
@@ -0,0 +1,5 @@
+enter letter grade(or'q'to quit):A
+enter letter grade(or'q'to quit):B
+enter letter grade(or'q'to quit):C
+enter letter grade(or'q'to quit):q
+your grade average for this class is:3.00
